6e1cf341c7c6d46a409d504ad5006e267230e9f5
MilleEmdal <millememdal@gmail.com>
2019-04-10 22:04:19 +0200

    hel

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+include('includes/header.php');
+include('includes/navigation.php'); ?>
+
+
+    <div class="container">
+        <div class="row">
+
+
+
+      <div class="card text-center">
+        <div class="card-header">
+          Dance
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Take a look at our dance </h5>
+          <p class="card-text">Dance have a lot a different things</p>
+          <a href="#" class="mybtn btn btn-primary">Dance ;)</a>
+        </div>
+        <div class="card-footer text-muted">
+          Let´s dance
+        </div>
+      </div>
+      
+        </div>
+    </div>
+
+
+
+<?php include('includes/footer.php'); ?>
